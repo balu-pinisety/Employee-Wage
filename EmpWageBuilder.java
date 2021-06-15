593bd10c9bde273fcfa0cc7b586f05427f55ea77
balu-pinisetty <balupinisetty@gmail.com>
2021-06-15 19:55:53 +0530

    Modified method to check Employee presence

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,5 +3,15 @@ public class EmpWageBuilder {
 	public static void main(String[] args) {
 		//Displaying Welcome Message
 		System.out.println("Welcome to Employee Wage Computation Problem");
+		//Constants
+		int IS_FULL_TIME=1;
+		//Computation
+		int empCheck = (int) Math.floor(Math.random()*10)%2;
+		if (empCheck == IS_FULL_TIME) {
+			System.out.println("Employee is Present");
+		}
+		else {
+			System.out.println("Employee is Absent");
+		}
 	}
 }
