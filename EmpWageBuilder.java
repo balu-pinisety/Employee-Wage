8a5bb14827342892755c4617a198c5b244f7a872
balu-pinisetty <balupinisetty@gmail.com>
2021-06-16 04:29:34 +0530

    Refactor the code into Switch Case

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,27 +1,32 @@
 public class EmpWageBuilder {
 
+	//Constants
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;
+	public static final int WAGE_PER_HOUR=20;
+	
 	public static void main(String[] args) {
 		//Displaying Welcome Message
 		System.out.println("Welcome to Employee Wage Computation Problem\n");
-		//Constants
-		int IS_FULL_TIME=1;
-		int IS_PART_TIME=2;
-		int WAGE_PER_HOUR=20;
 		//variables
 		int empWorkHrs;
-		//Computation
+		//Computation using Switch case
 		int empCheck = (int) Math.floor(Math.random()*10)%3;
-		if (empCheck == IS_FULL_TIME) {
-			System.out.println("Employee is Present\n");
-			empWorkHrs=8;
-		}
-		else if (empCheck == IS_PART_TIME) {
-			System.out.println("Employee is Part-time Present\n");
-			empWorkHrs=4;
-		}
-		else {
-			System.out.println("Employee is Absent\n");
-			empWorkHrs=0;
+		switch (empCheck) {
+			case IS_FULL_TIME: {
+				System.out.println("Employee is Present\n");
+				empWorkHrs=8;
+				break;
+			}
+			case IS_PART_TIME: { 
+				System.out.println("Employee is Part-time Present\n");
+				empWorkHrs=4;
+				break;
+			}
+			default: {
+				System.out.println("Employee is Absent\n");
+				empWorkHrs=0;
+			}
 		}
 		//Calculating Employee Daily Wage
 		int empWage=empWorkHrs*WAGE_PER_HOUR;
