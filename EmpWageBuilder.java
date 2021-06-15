f053f0563b1d6542434203ada7b86c15d65783cd
balu-pinisetty <balupinisetty@gmail.com>
2021-06-15 22:38:04 +0530

    Modified and Added Part time computation

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -5,14 +5,19 @@ public class EmpWageBuilder {
 		System.out.println("Welcome to Employee Wage Computation Problem\n");
 		//Constants
 		int IS_FULL_TIME=1;
+		int IS_PART_TIME=2;
 		int WAGE_PER_HOUR=20;
 		//variables
 		int empWorkHrs;
 		//Computation
-		int empCheck = (int) Math.floor(Math.random()*10)%2;
+		int empCheck = (int) Math.floor(Math.random()*10)%3;
 		if (empCheck == IS_FULL_TIME) {
 			System.out.println("Employee is Present\n");
 			empWorkHrs=8;
+		}
+		else if (empCheck == IS_PART_TIME) {
+			System.out.println("Employee is Part-time Present\n");
+			empWorkHrs=4;
 		}
 		else {
 			System.out.println("Employee is Absent\n");
