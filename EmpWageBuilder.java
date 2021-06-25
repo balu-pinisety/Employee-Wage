89c17cff48089500c2ae3ceddaae6e83fcb2d526
balu-pinisety <manikantapandu225@gmail.com>
2021-06-25 18:01:50 +0530

    Modified: Code to save the total wage fo each company

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,12 +3,30 @@ public class EmpWageBuilder {
 	//Constants
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
-
+	private final String company;
+	private final int monthHours;
+	private final int monthDays;
+	private final int wageRate;
+	private int totalWage;
 	public static void main(String[] args) {
-		computeWage("Company1",80, 25, 20);
-		computeWage("Company2",100, 30, 25);
+		//Creating objects for class
+		EmpWageBuilder Comp1=new EmpWageBuilder("Company1",80, 25, 20);
+		EmpWageBuilder Comp2=new EmpWageBuilder("Company2",100, 30, 25);
+		//Computing total wage
+		Comp1.computeWage();
+		System.out.println(Comp1);
+		Comp2.computeWage();
+		System.out.println(Comp2);
 	}
-	public static void computeWage(String Company, int monthHours, int monthDays, int wageRate) {
+	public EmpWageBuilder(String company, int monthHours, int monthDays, int wageRate) {
+		//Assigning to variables
+		this.company=company;
+		this.monthHours=monthHours;
+		this.monthDays=monthDays;
+		this.wageRate=wageRate;		
+	}
+	
+	public void computeWage() {
 		//variables
 		int empWorkHrs;
 		int totalWorkHrs=0;
@@ -36,7 +54,13 @@ public class EmpWageBuilder {
 				daysCount++;
 		}
 		//Calculating Employee Monthly Wage
-		int monthWage=totalWorkHrs*wageRate;
-		System.out.println("Total Employee Wage for "+Company+" is "+monthWage+"\n");
+		totalWage=totalWorkHrs*wageRate;
+//		System.out.println("Total Employee Wage for "+Company+" is "+monthWage+"\n");
+	}
+	
+	@Override
+	public String toString() {
+		//
+		return "Total Employee Wage for "+company+" is "+totalWage+"\n";
 	}
 }
