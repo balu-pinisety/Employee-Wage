03e1b7869d64323500be101ef9803c845bad2611
balu-pinisety <manikantapandu225@gmail.com>
2021-06-27 19:41:48 +0530

    Added: File for to implement methods in One

---
+++ InterfaceEmpWage.java
@@ -0,0 +1,11 @@
+package com.EmpWageComputing;
+
+/**
+ * To implement all methods in Employee wage Computation by overriding interface method
+ */
+public interface InterfaceEmpWage {
+	public void addCompanyWage(String company, int monthHours, int monthDays, int wageRate);
+	
+	public void computeWage();
+
+}
